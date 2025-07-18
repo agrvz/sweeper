5c516b26f40e6dcfda53f9751dd525bcd19a9ac5
agrvz <agrvzmail@gmail.com>
2025-07-19 00:44:06 +0100

    Add CSV load functions

--- src/sweeper/io.py
+++ src/sweeper/io.py
@@ -1,3 +1,4 @@
+import csv
 from pathlib import Path
 
 
@@ -10,3 +11,64 @@ def get_lines_from_file(filepath: Path) -> list:
     with open(filepath, "r") as in_file:
         lines = in_file.read().splitlines()
     return lines
+
+
+def load_csv_rows_as_lists(filepath: Path) -> list[list]:
+    """
+    Load a CSV file and return rows as lists. The CSV file must have a header row.
+    """
+    if isinstance(filepath, str):
+        filepath = Path(filepath)
+    with open(filepath, "r") as in_file:
+        if not in_file.read():
+            raise ValueError(f"CSV file {filepath} is empty.")
+
+        in_file.seek(0)
+        reader = csv.reader(in_file)
+        # Skip header row
+        next(reader)
+        return [row for row in reader if row]
+
+
+def load_csv_rows_as_dicts(filepath: Path) -> list[dict]:
+    """
+    Load a CSV file and return rows as dictionaries. The CSV file must have a header row.
+    """
+    if isinstance(filepath, str):
+        filepath = Path(filepath)
+    with open(filepath, "r") as in_file:
+        if not in_file.read():
+            raise ValueError(f"CSV file {filepath} is empty.")
+
+        in_file.seek(0)
+        reader = csv.DictReader(in_file)
+        return [row for row in reader if row]
+
+
+def load_csv(
+    *, filepath: Path, column_name: str | None = None, column_index: int | None = None
+) -> list:
+    """
+    Load a CSV file and return the contents of a single column as a list of strings.
+    You can specify the column to load either by name or by index.
+    """
+    if column_name and column_index:
+        raise ValueError("You must pass either column_name or column_index, not both.")
+
+    if column_name:
+        rows = load_csv_rows_as_dicts(filepath)
+        try:
+            return [row[column_name] for row in rows]
+        except KeyError:
+            raise ValueError(
+                f"Column '{column_name}' not found in one or more rows in file."
+            )
+
+    if column_index:
+        rows = load_csv_rows_as_lists(filepath)
+        try:
+            return [row[column_index] for row in rows]
+        except IndexError:
+            raise ValueError(
+                f"Column index {column_index} out of range for one or more rows in file."
+            )
